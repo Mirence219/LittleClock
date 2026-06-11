fe0aaee86ece219023a7ea7b0fbfcf9313959613
麦仁丝 <1211012802@qq.com>
2026-06-11 18:55:20 +0800

    支持Linux系统 1. 加入了对Linux系统的支持 2. 首个Linux版本发布

--- __main__.py
+++ __main__.py
@@ -1,10 +1,17 @@
+import os
+import sys
+
 from littleclock import LittleClock
 from __version__ import version, debug
 from logger import Logger
 
 if __name__ == "__main__":
-    Logger.set_level("DEBUG" if debug else "WARNING")   #配置日志等级
-    Logger.set_level("INFO")
+    if sys.platform == "linux": #linux系统Qt额外设置
+        os.environ["QT_QPA_PLATFORM"] = "xcb"
+        os.environ["QT_QUICK_BACKEND"] = "software"
+        os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = ""
+
+    Logger.set_level("INFO" if debug else "WARNING")   #配置日志等级
     
     print(f"LittleClock版本：{version}，模式：{"Debug"if debug else "Realse"}，日志等级：{Logger.level}",)
     application = LittleClock()
